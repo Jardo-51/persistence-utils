7a201e1c006d7213218b432cf4cd66547e2f2eda
Jaroslav Brtis <jaroslav.brtis@protonmail.com>
2019-07-31 13:10:28 +0200

    Added Javadoc.

--- src/main/java/com/jardoapps/persistence/utils/Defs.java
+++ src/main/java/com/jardoapps/persistence/utils/Defs.java
@@ -2,6 +2,9 @@ package com.jardoapps.persistence.utils;
 
 public final class Defs {
 
+	/**
+	 * Length of a String representation of UUID 4.
+	 */
 	public static final int UUID_LENGTH = 36;
 
 	private Defs() {
